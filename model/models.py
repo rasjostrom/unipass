6774244bb15ed2e5777f2eb0374d30c85a138e6b
John Herrlin <jherrlin@gmail.com>
2015-11-27 20:18:24 +0100

    Documentation of models.py made. User class added to models.py Python3 fix in db.py

--- model/models.py
+++ model/models.py
@@ -8,6 +8,48 @@ from db import Model
 from descriptors import Descriptor, GetDescriptor
 
 
+"""
+models.py
+
+This file is used to define persistance objects.
+The only this that you have to care abour here is to inherit from Model
+if creating a new persistance object.
+
+The descriptors is only used to talk to the "controller" layer and dont 
+have anything to do with persistance.
+
+the initdb() function will check in this file for classes and
+creates the database depending on the objects in here.
+there is no migration options here. 
+if made changes, remove database and run the application again.
+
+Example:
+class User(Model):  // Inherits from Model
+
+    uuid = GetDescriptor('_uuid')  // UUID = Universally unique identifier
+                                   // This uuid is the value that we use in database
+    
+    def __init__(self, **kwargs):  // Constructor
+        super(Model, self).__init__()  // Call parent
+        self._uuid = (kwargs['_uuid'] if '_uuid' in kwargs else str(uuid1()))  // <- This one is required!
+        self._name = (kwargs['_name'] if '_name' in kwargs else '')  // <- Optional
+        self._password = (kwargs['_password'] if '_password' in kwargs else '')  // <- Optional
+
+"""
+
+
+class User(Model):
+    """ User object
+    """
+    uuid = GetDescriptor('_uuid')
+    
+    def __init__(self, **kwargs):
+        super(Model, self).__init__()
+        self._uuid = (kwargs['_uuid'] if '_uuid' in kwargs else str(uuid1()))
+        self._name = (kwargs['_name'] if '_name' in kwargs else '')
+        self._password = (kwargs['_password'] if '_password' in kwargs else '')
+
+        
 class Entry(Model):
     """Password object.
     Name
