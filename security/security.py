35af2abd605912349891068fc511c816469e9b73
John Herrlin <jherrlin@gmail.com>
2015-12-02 19:45:31 +0100

    init files added to security package import and some fixes in security

--- security/security.py
+++ security/security.py
@@ -1,13 +1,16 @@
 
+from blowfish import Cipher as cipher
+
+
 class Crypto(object):
 
     def __init__(self, iv):
         self.iv = iv
 
-    def encrypt(data):
-        return data_encrypted = b"".join(cipher.encrypt_cbc(data, self.iv))
+    def encrypt(self, data):
+        return b"".join(cipher.encrypt_cbc(data, self.iv))
+
+    def decrypt(self, data):
+        return b"".join(cipher.decrypt_cbc(data, self.iv))
 
 
-    def decrypt(data):
-        return data_decrypted = b"".join(cipher.decrypt_cbc(data, self.iv))
-        
