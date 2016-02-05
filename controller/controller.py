0335bd48e3f86ea6ba32a01c553fc3ac7048de3e
jherrlin <jherrlin@gmail.com>
2016-02-05 22:32:06 +0100

    controller started

--- controller/controller.py
+++ controller/controller.py
@@ -1,1 +1,15 @@
 
+from model.models import User
+
+
+def create_admin(username, password):
+    user = User()
+    user.name = username
+    user.password = password
+    user.admin = True
+    if user.valid():
+        user.create()
+        return True
+    else:
+        return False
+
