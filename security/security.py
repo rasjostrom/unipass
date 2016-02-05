c94d1e4599ba5ecb528acf543478c6434237cb37
jherrlin <jherrlin@gmail.com>
2016-02-05 14:21:53 +0100

    security package functions now have key as arg

--- security/security.py
+++ security/security.py
@@ -1,16 +1,14 @@
-
 from blowfish import Cipher as cipher
 
 
 class Crypto(object):
 
-    def __init__(self, iv):
-        self.iv = iv
+    @staticmethod
+    def encrypt(self, data, key):
+        return str(b"".join(cipher.encrypt_cbc(data, key)))
 
-    def encrypt(self, data):
-        return b"".join(cipher.encrypt_cbc(data, self.iv))
-
-    def decrypt(self, data):
-        return b"".join(cipher.decrypt_cbc(data, self.iv))
+    @staticmethod
+    def decrypt(self, data, key):
+        return str(b"".join(cipher.decrypt_cbc(data, key)))
 
 
