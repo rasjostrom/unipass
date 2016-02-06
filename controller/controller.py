29e4c8e803fbd0885a1217f6e187e238bb424855
jherrlin <jherrlin@gmail.com>
2016-02-06 14:03:34 +0100

    controller updated to work with --add, --list and --get in console view

--- controller/controller.py
+++ controller/controller.py
@@ -2,7 +2,14 @@
 from model.models import User
 
 
-def create_admin(username, password):
+def login(username, password):
+    for user in User.getall():
+        if (user.name == username and user.password == password) and user.admin:
+            return True
+    return False
+
+
+def create_user(username, password):
     user = User()
     user.name = username
     user.password = password
@@ -12,4 +19,46 @@ def create_admin(username, password):
         return True
     else:
         return False
+
+def find_by_service(service):
+    for user in User.getall():
+        if user.service == service:
+            return user
+    return None
+
+
+def list_all_services():
+    return [(user.service, user.name) for user in User.getall()]
+
+
+def add_service(service, name, password, note):
+    serv = User()
+    serv.service = service
+    serv.name = name
+    serv.password = password
+    serv.note = note
+    if serv.valid():
+        serv.create()
+        return True
+    else:
+        return False
+
+
+def update_service(uuid, **kwargs):
+    serv = User.getbyuuid(uuid)
+    serv.service = kwargs['service']
+    serv.name = kwargs['name']
+    serv.password = kwargs['password']
+    serv.note = kwargs['note']
+    if serv.valid():
+        serv.update()
+        return True
+    else:
+        return False
+
+
+def get_service(uuid):
+    return User.getbyuuid(uuid)
+
+
 
