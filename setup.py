4e63dfba5064b5e11c136129efc1d6e34b756dc7
jherrlin <jherrlin@gmail.com>
2016-02-09 21:05:44 +0100

    some small fixes before released to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
-
+from 
 setup(
     name = 'unipass',
     packages = ['unipass'],
-    version = 0.1,
-    description = 'Password manager in python',
+    version = '0.1-alpha',
+    description = 'Password manager in Python',
     author = 'John Herrlin',
     author_email = 'jherrlin@gmail.com',
     url = 'https://github.com/jherrlin/unipass',
-    download_url = 'https://github.com/jherrlin/unipass/tarball/0.1',
+    download_url = 'https://github.com/jherrlin/unipass/tarball/0.1-alpha',
     keywords = ['password', 'manager']
     classifiers = []
 )
---
+++ unipass/__init__.py
@@ -0,0 +1,6 @@
+
+__version__ = '0.1-alpha'
+__title__ = 'unipass'
+__author__ = 'John Herrlin'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 John Herrlin'
