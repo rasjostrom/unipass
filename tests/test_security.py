2d77dd331e75848af7b818899250e14c09ade4f7
jherrlin <jherrlin@gmail.com>
2016-02-17 12:21:52 +0100

    AES security and tests for security added.
    Nothing is implemented yes, only testing.

--- tests/test_security.py
+++ tests/test_security.py
@@ -2,27 +2,21 @@
 
 import unittest
 
-from unipass.security.security import encrypt_and_pickle_dumps, decrypt_and_pickle_loads
+from unipass.security.security import AESCipher as aes
+from unipass.model.models import Service
+import json
+from pprint import pprint
+import ast
 
+class SecurityTest(unittest.TestCase):
 
-# class CryptoTest(unittest.TestCase):
-#
-#     def test_Crypto_Str_True(self):
-#         self.password = 'password'
-#         self.key = 'a'
-#         self.cipher_pickled = encrypt_and_pickle_dumps(self.password, self.key)
-#         self.assertEquals(self.password, decrypt_and_pickle_loads(self.cipher_pickled, self.key))
-
-    # def test_Crypto_StrSwe_True(self):
-    #     self.password = 'passwordåäöÅÄÖ'
-    #     self.key = 'a'
-    #     self.cipher_pickled = encrypt_and_pickle_dumps(self.password, self.key)
-    #     self.assertEquals(self.password, decrypt_and_pickle_loads(self.cipher_pickled, self.key))
-    #
-    # def test_Crypto_StrPol_True(self):
-    #     self.password = 'passwordidziewążwąskądróżką'
-    #     self.key = 'a'
-    #     self.cipher_pickled = encrypt_and_pickle_dumps(self.password, self.key)
-    #     self.assertEquals(self.password, decrypt_and_pickle_loads(self.cipher_pickled, self.key))
+    def test_AESCipher_ServiceObject_True(self):
+        self.user = Service(_name='John', _password='password', _service='gmail')
+        self.cipher = aes('my_key').encrypt(self.user.__dict__)
+        me = Service(**(aes('my_key').decrypt(self.cipher)))
+        self.assertEquals('John', me.name)
+        self.assertEquals('password', me.password)
+        self.assertEquals('gmail', me.service)
+        
 
 
--- unipass/security/security.py
+++ unipass/security/security.py
@@ -1,19 +1,31 @@
 
-import pickle
+import base64
+from Crypto import Random
+from Crypto.Cipher import AES
+import md5
+import ast
 
-from simplecrypt import encrypt, decrypt
+# http://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
+# http://code.runnable.com/UwdZzhi2XsttAAAU/crypto-example-for-python
 
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
+unpad = lambda s : s[:-ord(s[len(s)-1:])]
 
-def encrypt_and_pickle_dumps(data, key):
-    cipher = encrypt(data, key)
-    cipher_dump = pickle.dumps(cipher)
-    return cipher_dump
+class AESCipher(object):
+    
+    def __init__( self, key ):
+        self.key = md5.new(key).digest()
 
+    def encrypt( self, raw ):
+        raw = pad(str(raw))
+        iv = Random.new().read( AES.block_size )
+        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        return base64.b64encode( iv + cipher.encrypt( raw ) ) 
 
-def decrypt_and_pickle_loads(data, key):
-    cipher_dump = pickle.loads(data)
-    plaintext = decrypt(cipher_dump, key)
-    return plaintext
-
-
+    def decrypt( self, enc ):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        return ast.literal_eval(unpad(cipher.decrypt( enc[16:] )))
 
