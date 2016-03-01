0ca58d7cac9243b141dd31fb807f04a588323167
rasjostrom <ras.sjostrom@hotmail.com>
2016-03-01 20:45:51 +0100

    Fix encoding bug in security

--- unipass/security/security.py
+++ unipass/security/security.py
@@ -16,17 +16,20 @@ unpad = lambda s: s[:-ord(s[len(s)-1:])]
 class AESCipher(object):
     
     def __init__(self, key):
-        self.key = md5(key).digest()
+        self.key = md5(key.encode('utf-8')).digest()
 
     def encrypt(self, raw):
         raw = pad(str(raw))
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw))
+        x = base64.b64encode(iv + cipher.encrypt(raw))
+        print("\nX: " + str(x))
+        return x
 
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
+        print("\nENC: " + str(enc))
         iv = enc[:16]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return ast.literal_eval(unpad(cipher.decrypt(enc[16:])))
+        return ast.literal_eval(unpad(cipher.decrypt(enc[16:])).decode())
 
